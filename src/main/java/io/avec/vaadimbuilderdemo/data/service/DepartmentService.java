db031c240baddca78bbd04358fbf60da949d002e
Avec <avec112@gmail.com>
2020-11-15 00:55:44 +0100

    - Added @Profile dev - Added application-dev.properties - Removed dependency Vaadin Artur Exampledata - Added Department @OneToMany Person - Added Person @ManyToOne Department - Added ComboBox<Department> inside PersonView - Added populater methods in DepartmentService for ComboBox<Department> - Added Binding for ComboBox inside PersonView

--- src/main/java/io/avec/vaadimbuilderdemo/data/service/DepartmentService.java
+++ src/main/java/io/avec/vaadimbuilderdemo/data/service/DepartmentService.java
@@ -1,10 +1,14 @@
 package io.avec.vaadimbuilderdemo.data.service;
 
 import io.avec.vaadimbuilderdemo.data.entity.Department;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.vaadin.artur.helpers.CrudService;
 
+import java.util.stream.Stream;
+
+@Getter
 @Service
 public class DepartmentService extends CrudService<Department, Integer> {
 
@@ -14,9 +18,18 @@ public class DepartmentService extends CrudService<Department, Integer> {
         this.repository = repository;
     }
 
-    @Override
-    protected DepartmentRepository getRepository() {
-        return repository;
+    public Stream<Department> fetch(String filter, int offset, int limit) {
+        return repository.findAll().stream()
+                .filter(department -> filter == null || department.toString()
+                        .toLowerCase().startsWith(filter.toLowerCase()))
+                .skip(offset).limit(limit);
     }
 
+    public int count(String filter) {
+        return (int) repository.findAll().stream()
+                .filter(department -> filter == null || department.toString()
+                        .toLowerCase().startsWith(filter.toLowerCase()))
+                .count();
+
+    }
 }
--- src/main/java/io/avec/vaadimbuilderdemo/data/service/PersonService.java
+++ src/main/java/io/avec/vaadimbuilderdemo/data/service/PersonService.java
@@ -1,23 +1,28 @@
 package io.avec.vaadimbuilderdemo.data.service;
 
 import io.avec.vaadimbuilderdemo.data.entity.Person;
-
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.vaadin.artur.helpers.CrudService;
 
+
+@Getter
 @Service
 public class PersonService extends CrudService<Person, Integer> {
 
     private final PersonRepository repository;
+    private final DepartmentService departmentService;
 
-    public PersonService(@Autowired PersonRepository repository) {
+    public PersonService(@Autowired PersonRepository repository, DepartmentService departmentService) {
         this.repository = repository;
+        this.departmentService = departmentService;
     }
 
-    @Override
-    protected PersonRepository getRepository() {
-        return repository;
-    }
+
+//    @Override
+//    protected PersonRepository getRepository() {
+//        return repository;
+//    }
 
 }
